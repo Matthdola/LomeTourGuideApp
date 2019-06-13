98d97aa5292e818a69e0646bd7a7e4c939689132
Matthias DOLA <koami.dola@pythagoras-afrique.com>
2019-06-13 16:21:11 +0000

    Add images to drawables

--- app/src/main/java/com/example/dola/lometourguideapp/MainActivity.java
+++ app/src/main/java/com/example/dola/lometourguideapp/MainActivity.java
@@ -5,15 +5,22 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+    @BindView(R.id.sliding_tabs)
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = findViewById(R.id.viewpager);
+        ButterKnife.bind(this);
 
         // Create an adapter that will knows which fragment should be shown on each page
         SimpleFragmentViewPagerAdapter adapter = new SimpleFragmentViewPagerAdapter(getSupportFragmentManager());
@@ -21,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         // Set the adapter onto the view page
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 }
